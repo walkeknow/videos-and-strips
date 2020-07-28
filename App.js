e0b21863f9ad9d83110ca19d092cd702c8064ea4
walkeknow <kewalshah077@gmail.com>
2020-07-28 21:12:55 +0530

    Add Tab Navigation

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { Text, View, Platform } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import VideoView from './components/videos/VideoView'
+import StripView from './components/strips/StripView'
+import { Ionicons } from '@expo/vector-icons'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Tab = createBottomTabNavigator()
+
+class App extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName
+
+              if (route.name === 'Videos') {
+                if (Platform.OS === 'ios')
+                  iconName = focused ? 'ios-play-circle' : 'ios-play'
+                else iconName = focused ? 'md-play-circle' : 'md-play'
+              } else if (route.name === 'Chemicals') {
+                if (Platform.OS === 'ios') iconName = 'ios-flask'
+                else iconName = 'md-flask'
+              }
+
+              return <Ionicons name={iconName} size={size} color={color} />
+            },
+          })}
+        >
+          <Tab.Screen name='Videos' component={VideoView} />
+          <Tab.Screen name='Chemicals' component={StripView} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
---
+++ components/videos/VideoView.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export class VideoView extends Component {
+  render() {
+    return (
+      <View>
+      </View>
+    )
+  }
+}
+
+export default VideoView
